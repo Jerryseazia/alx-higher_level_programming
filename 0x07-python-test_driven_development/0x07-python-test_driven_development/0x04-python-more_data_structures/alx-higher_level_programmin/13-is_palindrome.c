d07bbeebf56654e73be2294d61004b5533a090cd
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-06-06 16:03:04 -0700

    update

---
+++ 0x07-python-test_driven_development/0x07-python-test_driven_development/0x04-python-more_data_structures/alx-higher_level_programmin/13-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stddef.h>
+/**
+ * is_palindrome - prototype of the function
+ * @head: pointer to head of singly linked list
+ * Return: 0 if not, 1 if palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+listint_t *tmp = *head;
+unsigned int size = 0, x = 0;
+int data[10240];
+if (head == NULL)
+return (0);
+if (*head == NULL)
+return (1);
+while (tmp)
+{
+tmp = tmp->next;
+size += 1;
+}
+if (size == 1)
+return (1);
+tmp = *head;
+while (tmp)
+{
+data[x++] = tmp->n;
+tmp = tmp->next;
+}
+for (x = 0; x <= (size/2); x++)
+{
+if (data[x] != data[size - x - 1])
+return (0);
+}
+return (1);
+}
