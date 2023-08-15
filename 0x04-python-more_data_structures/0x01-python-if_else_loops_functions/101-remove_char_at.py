600ac1ee4ececb6d37cf7a470714c5d1b237931a
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-08-15 15:26:29 -0700

    Javascript Object

--- 0x04-python-more_data_structures/0x01-python-if_else_loops_functions/101-remove_char_at.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def remove_char_at(str, x):
-    if x >= 0:
-        return str[:x] + str[x+1:]
-    else:
-        return str
---
+++ 9-logme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+let numb = -1;
+exports.logMe = function (item) {
+  numb++;
+  console.log(numb + ': ' + item);
+};#!/usr/bin/node
+
+let numb = -1;
+exports.logMe = function (item) {
+  numb++;
+  console.log(numb + ': ' + item);
+};
